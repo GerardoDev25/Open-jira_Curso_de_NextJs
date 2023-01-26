e7e332b71cb5e5c24b77f55bd10f98b47a0e1cbf
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-26 12:46:35 -0400

    5. EntriesContext - Contexto de las entradas

---
+++ context/entries/EntriesProvider.tsx
@@ -0,0 +1,28 @@
+import { useReducer } from 'react';
+import { EntriesContext,  entriesReducer } from './';
+
+export interface EntriesState {
+  entries: [];
+}
+
+const ENTRIES_INITIAL_STATE: EntriesState = {
+  entries: [],
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const EntriesProvider: React.FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(entriesReducer, ENTRIES_INITIAL_STATE);
+
+  return (
+    <EntriesContext.Provider
+      value={{
+        ...state,
+      }}
+    >
+      {children}
+    </EntriesContext.Provider>
+  );
+};

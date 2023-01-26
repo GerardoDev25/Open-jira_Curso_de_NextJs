899e5bf839739a88c2acbb45cbb25dd2cb2c9e46
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-26 13:28:35 -0400

    6. Interface Entry

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,12 +1,34 @@
 import { useReducer } from 'react';
-import { EntriesContext,  entriesReducer } from './';
+import { v4 as UUID } from 'uuid';
+
+import { EntriesContext, entriesReducer } from './';
+import { Entry } from '@/interfaces';
 
 export interface EntriesState {
-  entries: [];
+  entries: Entry[];
 }
 
 const ENTRIES_INITIAL_STATE: EntriesState = {
-  entries: [],
+  entries: [
+    {
+      _id: UUID(),
+      description: 'Consequat eiusmod aliqua magna nostrud reprehenderit.',
+      status: 'pending',
+      createAt: Date.now(),
+    },
+    {
+      _id: UUID(),
+      description: 'Consequat eiusmod aliqua magna nostrud reprehenderit.',
+      status: 'in-progress',
+      createAt: Date.now() -1_000_000,
+    },
+    {
+      _id: UUID(),
+      description: 'Consequat eiusmod aliqua magna nostrud reprehenderit.',
+      status: 'finished',
+      createAt: Date.now() - 100_000,
+    },
+  ],
 };
 
 interface Props {

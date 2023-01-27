fe0942447a9f6ba138e11044c6f0d7af138a01f2
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-27 11:16:38 -0400

    10. Trabajar con nuestro formulario y estado

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,31 +1,72 @@
+import { useState } from 'react';
 import { Button, TextField } from '@mui/material';
 import { Box } from '@mui/system';
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 export const NewEntry = () => {
+  const [isAdding, setIsAdding] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [touched, setTouched] = useState(false);
+
+  const onTextFielChanges = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSave = () => {
+    if (inputValue.length === 0) return;
+    console.log(inputValue);
+  };
+
+  const onCancel = () => {
+    setIsAdding(false);
+    setTouched(false);
+    setInputValue('');
+  };
+
   return (
     <Box sx={{ marginBottom: 2, paddingX: 1 }}>
-      <Button startIcon={<AddCircleOutlineOutlinedIcon />} fullWidth variant='outlined'>
-        Agregar tarea
-      </Button>
-      <TextField
-        fullWidth
-        sx={{
-          marginTop: 2,
-          marginBottom: 1,
-        }}
-        autoFocus
-        multiline
-        label='Nueva entrada'
-        helperText='ingrese texto'
-      />
-      <Box display={'flex'} justifyContent='space-between'>
-        <Button variant='outlined'>Cancelar</Button>
-        <Button variant='outlined' color='secondary' endIcon={<SaveOutlinedIcon />}>
-          Guardar
+      {isAdding ? (
+        <>
+          <TextField
+            fullWidth
+            sx={{
+              marginTop: 2,
+              marginBottom: 1,
+            }}
+            autoFocus
+            multiline
+            label='Nueva entrada'
+            helperText={inputValue.length <= 0 && touched && 'ingrese texto'}
+            error={inputValue.length <= 0 && touched}
+            value={inputValue}
+            onChange={onTextFielChanges}
+            onBlur={() => setTouched(true)}
+          />
+          <Box display={'flex'} justifyContent='space-between'>
+            <Button variant='outlined' onClick={onCancel}>
+              Cancelar
+            </Button>
+            <Button
+              variant='outlined'
+              color='secondary'
+              endIcon={<SaveOutlinedIcon />}
+              onClick={onSave}
+            >
+              Guardar
+            </Button>
+          </Box>
+        </>
+      ) : (
+        <Button
+          startIcon={<AddCircleOutlineOutlinedIcon />}
+          fullWidth
+          variant='outlined'
+          onClick={() => setIsAdding(true)}
+        >
+          Agregar tarea
         </Button>
-      </Box>
+      )}
     </Box>
   );
 };

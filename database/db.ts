603524b07a57c97b7e98e9950e93f3600fc52815
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-28 10:27:58 -0400

    6. Conectarnos a MongoDB con Mongoose

---
+++ database/db.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+// 0 = disconected
+// 1 = conected
+// 2 = conecting
+// 3 = diconecting
+
+const mongoConnection = {
+  isConected: 0,
+};
+
+export const conecct = async () => {
+  if (mongoConnection.isConected) {
+    console.log('ya estamos conectados');
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    mongoConnection.isConected = mongoose.connections[0].readyState;
+
+    if (mongoConnection.isConected === 1) {
+      console.log('usendo conecion anterior');
+      return;
+    }
+    await mongoose.disconnect();
+  }
+
+  await mongoose.connect('');
+  mongoConnection.isConected = 1;
+  console.log('conectado a mongodb');
+};
+
+export const disconnect = async () => {
+  if (mongoConnection.isConected !== 0) return;
+
+  await mongoose.disconnect();
+  console.log('Deconectando de mongoDb');
+};

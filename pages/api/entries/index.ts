75fb71c9d82d5d3b4b21e7ebee66850664f42217
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-29 14:47:58 -0400

    13. Crear una nueva entrada - Backend

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -3,12 +3,15 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { db } from '@/database';
 import { EntryModel, IEntry } from '@/models';
 
-type Data = { message: string } | IEntry[];
+type Data = { message: string } | IEntry[] | IEntry;
 
 export default function handle(req: NextApiRequest, res: NextApiResponse<Data>) {
   switch (req.method) {
     case 'GET':
       return getEntries(res);
+
+    case 'POST':
+      return postEntries(req, res);
 
     default:
       return res.status(400).json({ message: 'endpoint not exist' });
@@ -21,4 +24,21 @@ const getEntries = async (res: NextApiResponse<Data>) => {
   await db.disconnect();
 
   return res.status(200).json(entries);
+};
+
+const postEntries = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { description = '' } = req.body;
+
+  const newEntry = new EntryModel({ description, createAt: Date.now() });
+
+  try {
+    await db.conecct();
+    await newEntry.save();
+    await db.disconnect();
+    return res.status(201).json(newEntry);
+  } catch (error) {
+    await db.disconnect();
+    console.log(error);
+    return res.status(500).json({ message: 'something was wrong check server log' });
+  }
 };

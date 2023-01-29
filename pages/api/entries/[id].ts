4a5083961def4591b6da5e9461baee75c14535ec
Gerardo Miranda <gerardodev25@gmail.com>
2023-01-29 15:20:37 -0400

    15. Actualizar una entrada en particular - Backend

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,46 @@
+import { db } from '@/database';
+import { EntryModel, IEntry } from '@/models';
+import mongoose from 'mongoose';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = { message: string } | IEntry;
+
+export default function handle(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { id } = req.query;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ message: `id: ${id} invalid` });
+  }
+
+  switch (req.method) {
+    case 'PUT':
+      return updateEntry(req, res);
+
+    default:
+      return res.status(400).json({ message: `method not exits` });
+  }
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query;
+
+  await db.conecct();
+
+  const entryToUpdate = await EntryModel.findById(id);
+
+  if (!entryToUpdate) {
+    await db.disconnect();
+    return res.status(400).json({ message: `entry not exits` });
+  }
+
+  const { description = entryToUpdate.description, status = entryToUpdate.status } = req.body;
+
+  const updatedEntry = await EntryModel.findByIdAndUpdate(
+    id,
+    { description, status },
+    { runValidators: true, new: true }
+  );
+
+  await db.disconnect();
+  return res.status(200).json(updatedEntry!);
+};
